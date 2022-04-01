345d1266dfe89f576a69c85167e710f83da4cda9
lewis <tommy2005197@outlook.com>
2022-04-01 22:59:36 +0800

    fix

---
+++ chap5/readme.md
@@ -0,0 +1,8 @@
+# 命令
+make type.bin 
+# 类型：
+- db 100
+- dw 0xaa55
+- dd 0x12345678
+- db "hello "
+
---
+++ chap5/type.asm
@@ -0,0 +1,5 @@
+db 100; define byte 
+dw 0xaa55; define two byte 
+dd  0x12345678; define four byte 
+
+db "hello world!!!" ; define string 
